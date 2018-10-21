ed572892a66f093c130bfe37df802d3c4dda09e0
G1020BomberSquad <johan01.meer@gmail.com>
2018-10-21 17:41:56 +0200

    Functional game
    You can now play the game.
    Need to add a working scoreboard, and that you can't click the same card twice

--- SpellenScherm/SpellenScherm/MainWindow.xaml.cs
+++ SpellenScherm/SpellenScherm/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,24 +30,59 @@ namespace SpellenScherm
 
         private void menu_Click(object sender, RoutedEventArgs e)
         {
-
+            menuBar.Visibility = Visibility.Visible;
         }
 
         private void start_Click(object sender, RoutedEventArgs e)
         {
             grid = new MemoryGrid(GameGrid, 4, 4);
+            start.Visibility = Visibility.Collapsed;
         }
 
-        private void insertName1_Click(object sender, RoutedEventArgs e)
-        {           
-            string userName1 = nameEnter1.Text;
-            name1.Content = userName1;
-        }
-
-        private void insertName2_Click(object sender, RoutedEventArgs e)
+        private void setNames_Click(object sender, RoutedEventArgs e)
         {
+            string userName1 = nameEnter1.Text;
             string userName2 = nameEnter2.Text;
+            name1.Content = userName1;
             name2.Content = userName2;
+            set1.Visibility = Visibility.Collapsed;
+            set2.Visibility = Visibility.Collapsed;
+        }
+
+        private void SaveGame_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You can't save your game yet.");
+        }
+
+        private void LoadGame_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You can't load a game yet.");
+        }
+
+        private void ResetGame_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+        }
+
+        private void toMain_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("A main screen has not been made yet.");
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://nl.wikipedia.org/wiki/Memory");
+        }
+
+        private void CloseMenuBar_Click(object sender, RoutedEventArgs e)
+        {
+            menuBar.Visibility = Visibility.Collapsed;
         }
     }
 }
--- SpellenScherm/SpellenScherm/MemoryGrid.cs
+++ SpellenScherm/SpellenScherm/MemoryGrid.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -10,11 +10,16 @@ using System.Windows.Media.Imaging;
 
 namespace SpellenScherm
 {
-     public class MemoryGrid
+    public class MemoryGrid
     {
         private Grid grid;
         private int rows, cols;
-        private object game_grid;
+        static int numberOfClicks = 0;
+        static int score;
+        private bool hasDelay;
+        private Image card;
+        private Image Image1;
+        private Image Image2;
 
         public MemoryGrid(Grid grid, int rows, int cols)
         {
@@ -26,7 +31,9 @@ namespace SpellenScherm
             AddImages();
         }
 
-
+        /// <summary>
+        /// Initialises the GameGrid
+        /// </summary>
         private void InitializeGrid()
         {
             for (int i = 0; i < rows; i++)
@@ -38,7 +45,10 @@ namespace SpellenScherm
                 grid.ColumnDefinitions.Add(new ColumnDefinition());
             }
         }
-
+        
+        /// <summary>
+        /// Adds images to the grid
+        /// </summary>
         private void AddImages()
         {
             List<ImageSource> images = GetImagesList();
@@ -49,45 +59,170 @@ namespace SpellenScherm
                     Image back = new Image();
                     back.Source = new BitmapImage(new Uri("/images/back.png", UriKind.Relative));
 
-                    // klik op image = omdraaien
                     back.MouseDown += new System.Windows.Input.MouseButtonEventHandler(CardClick);
 
                     back.Tag = images.First();
                     images.RemoveAt(0);
-
                     Grid.SetColumn(back, col);
                     Grid.SetRow(back, row);
                     grid.Children.Add(back);
                 }
             }
-
         }
 
-        private void checkPoint()
-        {
-
-        }
-
-        private void CardClick(object sender, MouseButtonEventArgs e)
-        {
-            Image card = (Image)sender;
-            ImageSource front = (ImageSource)card.Tag;
-            card.Source = front;
-        }
-
-        private List<ImageSource> GetImagesList()
+        /// <summary>
+        /// Give each card a random image
+        /// </summary>
+        /// <returns>Return the images</returns>
+        public List<ImageSource> GetImagesList()
         {
             List<ImageSource> images = new List<ImageSource>();
+            List<string> random1 = new List<string>();
+            List<string> random2 = new List<string>();
 
             for (int i = 0; i < 16; i++)
             {
-                int imageNR = i % 8 + 1;
-                ImageSource source = new BitmapImage(new Uri("images/" + imageNR + ".png", UriKind.Relative));
-                images.Add(source);
-            }
+                if (i < 8)
+                {
+                    int imageNR = 0;
 
+                    Random rnd = new Random();
+                    imageNR = rnd.Next(1, 9);
+                    if (random1.Contains(Convert.ToString(imageNR)))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        random1.Add(Convert.ToString(imageNR));
+                        ImageSource source = new BitmapImage(new Uri("images/" + imageNR + ".png", UriKind.Relative));
+                        images.Add(source);
+                    }
+                }
+                if (i >= 8)
+                {
+                    int imageNR = 0;
+
+                    Random rnd = new Random();
+                    imageNR = rnd.Next(1, 9);
+                    if (random2.Contains(Convert.ToString(imageNR)))
+                    {
+                        i--;
+                    }
+                    else
+                    {
+                        random2.Add(Convert.ToString(imageNR));
+                        ImageSource source = new BitmapImage(new Uri("images/" + imageNR + ".png", UriKind.Relative));
+                        images.Add(source);
+                    }
+                }
+            }
             return images;
         }
 
+        /// <summary>
+        /// Turns the card when clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CardClick(object sender, MouseButtonEventArgs e)
+        {
+            if (hasDelay) return;
+
+            Image card = (Image)sender;
+            ImageSource front = (ImageSource)card.Tag;
+            card.Source = front;
+            numberOfClicks++;
+
+            checkCards(card);
+        }
+
+        /// <summary>
+        /// Grab the clicked card
+        /// </summary>
+        /// <param name="card">The card that has been clicked</param>
+        private void checkCards(Image card)
+        {
+
+            this.card = card;
+            if (numberOfClicks < 2 || numberOfClicks == 2)
+            {
+
+                if (this.Image1 == null)
+                {
+                    Image1 = card;
+                }
+                else if (this.Image2 == null)
+                {
+                    Image2 = card;
+                }
+            }
+
+            if (numberOfClicks == 2)
+            {
+                checkPair(Image1, Image2);
+
+                numberOfClicks = 0;
+                Image1 = null;
+                Image2 = null;
+            }
+        }
+
+        /// <summary>
+        /// Check if the two clicked cards have the same image (source)
+        /// </summary>
+        /// <param name="card1">The first card that has been clicked</param>
+        /// <param name="card2">The second card that has been clicked</param>
+        public void checkPair(Image card1, Image card2)
+        {
+            this.Image1 = card1;
+            this.Image2 = card2;
+
+            if (Convert.ToString(card1.Source) == Convert.ToString(card2.Source))
+            {
+                getPoint(card1, card2);
+            }
+            else
+            {
+                resetCards(Image1, Image2);
+            }
+
+        }
+
+        /// <summary>
+        /// Give points and remove pairs from GameGrid
+        /// </summary>
+        /// <param name="card1">The first card that has been clicked</param>
+        /// <param name="card2">The second card that has been clicked</param>
+        private async void getPoint(Image card1, Image card2)
+        {
+            score++;
+            hasDelay = true;
+            await Task.Delay(300);
+
+            card1.Source = new BitmapImage(new Uri("", UriKind.Relative));
+            card2.Source = new BitmapImage(new Uri("", UriKind.Relative));
+
+            hasDelay = false;
+        }
+
+        /// <summary>
+        /// Reset the cards
+        /// </summary>
+        /// <param name="card1">The first card that has been clicked</param>
+        /// <param name="card2">The second card that has been clicked</param>
+        private async void resetCards(Image card1, Image card2)
+        {
+            this.Image1 = card1;
+            this.Image2 = card2;
+
+            hasDelay = true;
+            await Task.Delay(1000);
+
+
+            card1.Source = new BitmapImage(new Uri("/images/back.png", UriKind.Relative));
+            card2.Source = new BitmapImage(new Uri("/images/back.png", UriKind.Relative));
+            hasDelay = false;
+        }
     }
 }
